61da834bd93b26abbb275ed085dfc1b3a0d9e45b
Danilo Maia <danilo.maia@discente.ufma.br>
2025-03-11 19:21:11 -0300

    cors config

--- src/main/java/com/labprog/PortalEgressos/config/SecurityConfig.java
+++ src/main/java/com/labprog/PortalEgressos/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,8 +35,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/authenticate", "/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
